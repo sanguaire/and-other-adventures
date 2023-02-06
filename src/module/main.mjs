2cfd9c514e8530df1d941ceaaf7edfd7a758027e
Andre Kurth <kurth.andre@scheidt-bachmann.de>
2023-02-06 09:28:44 +0100

    - adding CONST - moving sheet to other directory

--- src/module/main.mjs
+++ src/module/main.mjs
@@ -1,11 +1,19 @@
 import {ActorProxy} from "./documents/actor-proxy.mjs";
 import {ItemProxy} from "./documents/item-proxy.mjs";
+import {PcSheet} from "./sheets/actors/pc-sheet.mjs";
+import {MonsterSheet} from "./sheets/actors/monster-sheet.mjs";
+import {CONST} from "./const.mjs";
 
 Hooks.once("init", () => {
     CONFIG.Actor.documentClass = ActorProxy;
     CONFIG.Item.documentClass = ItemProxy;
 
     Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet(CONST.MODULE_NAME, PcSheet, {types: ["pc"], makeDefault: true})
+    Actors.registerSheet(CONST.MODULE_NAME, MonsterSheet, {types: ["monster"], makeDefault: true})
+
     Items.unregisterSheet("core", ItemSheet);
+
+
 });
 
